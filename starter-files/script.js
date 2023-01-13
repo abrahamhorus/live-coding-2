a64694affe16b8075cbabbf5c7339423c651e3a5
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-01-13 12:47:39 -0600

    se hizo lo que se pudo

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,49 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
+//********************************
+//*** Obteniendo datos con fetch
+
+var boton = document.getElementById('button');
+var contenedor = document.getElementById('input');
+var answer= document.getElementById("answer");
+var posts = null;
+
+boton.addEventListener('click', function () {
+
+    fetch('https://yesno.wtf/api')
+    .then(data => data.json())
+    .then(data => {
+        posts = data;
+        console.log("Funciona")
+       answer.innerHTML = data.answer;
+       
+    })
+
+});
+
+
+
+function handleKeyEnter(event) {
+    if (event.key === 'Enter') {
+      // Get the input's value
+      const input = document.querySelector('#input');
+      const value = input.value;
+      
+      // Do something with the value
+      console.log(value);
+    }
+  }
+
+  
+  var myTimeout = setTimeout(window.clearall, 3000);
+
+
+
+
+
+
+
 /**
  * STEPS:
  *
